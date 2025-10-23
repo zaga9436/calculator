1d8934f3354f87a183316240884f41f0d11c9615
zaga9436 <zaga9436@gmail.com>
2025-10-23 14:53:20 +0330

    готово

---
+++ script.js
@@ -0,0 +1,65 @@
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.number-btn');
+const operatorButtons = document.querySelectorAll('.operator-btn');
+const clearButton = document.querySelector('[data-value="clear"]');
+const equalsButton = document.querySelector('[data-value="equals"]');
+
+
+let currentInput = '';
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.dataset.value;
+        currentInput += value;
+        if (operator) {
+            display.value = firstValue + ' ' + operator + ' ' + currentInput;
+        } else {
+            display.value = currentInput;
+        }
+
+    });
+});
+
+let firstValue = '';
+let operator = '';
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.dataset.value;
+        firstValue = currentInput;
+        operator = value;
+        currentInput = '';
+        display.value = firstValue + ' ' + operator;
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    const secondValue = currentInput;
+
+    if (firstValue && operator && secondValue) {
+        let result;
+        const num1 = parseFloat(firstValue);
+        const num2 = parseFloat(secondValue);
+
+        if (operator === '+'){
+            result = num1 + num2;
+        } else if (operator === '-'){
+            result = num1 - num2;
+        } else if (operator === '*') {
+            result = num1 * num2;
+        } else {
+            result = num1 / num2;
+        }
+        display.value = result;
+        firstValue = '';
+        operator = '';
+        currentInput = result.toString();
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    currentInput = '';
+    firstValue = '';
+    operator = '';
+    display.value = '';
+})
